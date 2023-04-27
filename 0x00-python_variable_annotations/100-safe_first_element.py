ecb282bdb50b26e2671e898cdb0d61313db9a0e9
Nicholas <nicholasikiroma@gmail.com>
2023-04-27 23:45:09 +0100

    added doc to function

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -7,6 +7,7 @@ from typing import Sequence, Any, Union
 
 # The types of the elements of the input are not known
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """return first item or none"""
     if lst:
         return lst[0]
     else:
