21d13d3ef812fe561a0c414c492676c5b58cc95e
Nicholas <nicholasikiroma@gmail.com>
2023-04-27 20:54:08 +0100

    task two

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" type-annotated function that takes a string str1 and
+    a string str2 as arguments and
+    returns a concatenated string
+"""
+
+def concat(str1: str, str2: str) -> str:
+    """adds two numbers"""
+    return str1 + str2
