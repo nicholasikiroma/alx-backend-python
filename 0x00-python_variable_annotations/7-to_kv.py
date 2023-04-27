ecebdb08648e3560a61989bf967415f2328edab1
Nicholas <nicholasikiroma@gmail.com>
2023-04-27 23:33:39 +0100

    fixed type annotation

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -13,6 +13,6 @@ as a float.
 from typing import Tuple, Union
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple[str, Union[int, float]]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """return tuple"""
-    return (k, v)
+    return (k, v**2)
